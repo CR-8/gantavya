23d47fce635a8e17e9ec9ac5540ce8d8a600c140
Pratyush Tiwari <pratyush_be23ds043@srmcem.ac.in>
2025-10-13 16:03:49 +0530

    sync

--- src/components/home/bgforeground.tsx
+++ src/components/home/bgforeground.tsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function Bgforeground() {
   return (
-    <div className='min-h-screen w-screen z-20 bg-transparent -mt-12 flex items-center justify-center flex-col text-neutral-200'>
-      <span className='font-barbara text-[18vw]'>GANTAVYA</span>
-      <span className='-mt-8 font-bricolage text-4xl '>TECH PREMIER OF SRMCEM</span>
+    <div className="min-h-screen w-screen z-20 bg-transparent flex flex-col items-center justify-center text-neutral-200">
+      {/* Title */}
+      <div className="w-screen flex flex-col items-center justify-center mb-2">
+        <span className="font-barbara text-[18vw]">GANTAVYA</span>
+        <span className="-mt-8 font-bricolage text-4xl">
+          TECH PREMIER OF SRMCEM
+        </span>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Bgforeground
+export default Bgforeground;
